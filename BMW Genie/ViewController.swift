d0d83c6cfb02d23c459a9931eb942b8dbfb554d5
Shrikar Archak <shrikar@viralheat.com>
2015-01-10 18:22:59 -0800

    Demo app setup..

--- BMW Genie/ViewController.swift
+++ BMW Genie/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import MapKit
-
+import CoreLocation
 
 class ViewController: UIViewController {
 
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     let orange = UIColor(red: 253.0/255, green : 190.0/255, blue: 74.0/255, alpha: 1)
     let red = UIColor(red: 215.0/255 , green: 118.0/255, blue: 93.0/255, alpha: 1)
     
+    let locationManager = CLLocationManager()
+    @IBOutlet weak var mapView: MKMapView!
     
     
     @IBOutlet weak var diagnosticsView: UIView!
@@ -28,24 +30,52 @@ class ViewController: UIViewController {
     var presented: Bool = false
     @IBOutlet weak var fuelLabel: UILabel!
     @IBOutlet weak var batteryLabel: UILabel!
+    @IBOutlet weak var dLabel: UILabel!
+    var currLat: Double = 37.763785
+    var currLong: Double = -122.458825
     override func viewDidLoad() {
         super.viewDidLoad()
         var timer = NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: Selector("update"), userInfo: nil, repeats: true)
+        locationManager.requestAlwaysAuthorization()
+        println("Request when in use authorization")
     }
 
 
     @IBAction func takeMeThere(sender: AnyObject) {
         println("Take me there")
-        let latitude = 37.566150665283200
-        let longitude = -122.324226379394530
-        let placemark  = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: latitude, longitude: longitude), addressDictionary: nil)
-        let mapItem = MKMapItem(placemark: placemark)
-        mapItem.name = "Charing Station"
-        let options = [
+        
+        let strurl = "http://104.131.110.143:9000/find_station?latitude=\(currLat)&longitude=\(currLong)&radiusInMiles=10"
+        println(strurl)
+        let baseURL = NSURL(string: strurl)
+        let request = NSMutableURLRequest(URL: baseURL!)
+        let downloadTask = session.downloadTaskWithRequest(request, completionHandler: { (location, response, error) -> Void in
+            var tLat : Double = self.currLat
+            var tLong: Double = self.currLong
+            var name = "Charging station"
+            if(error == nil){
+                let objectData = NSData(contentsOfURL: location)
+                println(objectData)
+                let tmpData :NSString = NSString(data: objectData!, encoding: NSUTF8StringEncoding)!
+                var err: NSError
+                var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(objectData!, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+                
+                println(jsonResult)
+                name  = jsonResult.objectForKey("stationName") as String
+                tLat = jsonResult.objectForKey("latitude") as Double
+                tLong = jsonResult.objectForKey("longitude") as Double
+
+            }
+            let placemark  = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: tLat, longitude: tLong), addressDictionary: nil)
+            let mapItem = MKMapItem(placemark: placemark)
+            mapItem.name = name
+            let options = [
                 MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving,
                 MKLaunchOptionsShowsTrafficKey: false
             ]
-        mapItem.openInMapsWithLaunchOptions(options)
+            mapItem.openInMapsWithLaunchOptions(options)
+            
+        })
+        downloadTask.resume()
     }
     func getColor(value : Int) -> UIColor {
         let color = UIColor.greenColor()
@@ -79,9 +109,9 @@ class ViewController: UIViewController {
                     level = jsonResult.objectForKey("LastBatteryLevel") as Int
                 }
 
-                println("Battery level : \(level)")
+//                println("Battery level : \(level)")
                 let fuellevel = jsonResult.objectForKey("FuelLevel") as Int
-                println("Fuel level : \(fuellevel)")
+  //              println("Fuel level : \(fuellevel)")
 
 
                     dispatch_async(dispatch_get_main_queue(), { () -> Void in
@@ -99,7 +129,34 @@ class ViewController: UIViewController {
                         UIView.animateWithDuration(1.0, animations: { () -> Void in
                             self.batteryView.backgroundColor = self.getColor(level)
                             self.fuelView.backgroundColor = self.getColor(fuellevel)
+                            self.batteryLabel.text = "\(level)%"
+                            self.fuelLabel.text = "\(fuellevel)%"
                             
+                            
+                            if (jsonResult.objectForKey("DiagnosticCodes") != nil) {
+                                let dg = jsonResult.objectForKey("DiagnosticCodes") as NSDictionary
+                                let code = (dg.objectForKey("Codes") as NSArray)[0] as String
+                                println(code)
+                                if (code == "PB0118") {
+                                    self.diagnosticsView.backgroundColor = self.getColor(10)
+                                    self.dLabel.text = "Engine Coolant Temperature Sensor 1 Circuit High"
+                                }
+                            } else {
+                                println("XX its nil")
+                            }
+
+                            
+                            let myLoc = jsonResult.objectForKey("LastLocation") as NSDictionary
+                            let mapLat = myLoc.objectForKey("Lat") as Double
+                            let mapLng = myLoc.objectForKey("Lng") as Double
+                            self.currLat = mapLat
+                            self.currLong = mapLng
+                            let coordinate = CLLocationCoordinate2D(latitude: mapLat, longitude: mapLng)
+                            self.mapView.centerCoordinate = coordinate
+                            let reg = MKCoordinateRegionMakeWithDistance(coordinate, 5000, 5000)
+                            self.mapView.setRegion(reg, animated: true)
+                            self.mapView.showsUserLocation = true
+
                         })
                     })
 
